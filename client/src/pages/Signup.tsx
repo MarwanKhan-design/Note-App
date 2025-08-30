d879753196553e21a40ee7ecd8d311fcdffa86dd
Marwan <marwanpsh@gmail.com>
2025-08-30 23:17:15 +0500

    signup design complete

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import { useAuthStore } from "../store/authStore"
+import styles from "../styles/signup.module.css"
 
 const Signup = () => {
-    const [email, setEmail] = useState('')
-    const [name, setName] = useState('')
+    const [email, setEmail] = useState("")
+    const [name, setName] = useState("")
     const [OTPSent, setOTPSent] = useState(false)
-    const [OTP, setOTP] = useState('')
+    const [OTP, setOTP] = useState("")
 
     const { signup, message, getToken } = useAuthStore() as any
 
@@ -20,19 +21,55 @@ const Signup = () => {
         await getToken(parseInt(OTP), email)
     }
 
-    return <>
-        <form>
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="name" />
-            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+    return (
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <h2>Sign up</h2>
+                <p>Sign up to enjoy the feature of HD</p>
 
-            {OTPSent ? <>
-                <input type="number" value={OTP} onChange={(e) => setOTP(e.target.value)} />
-                <button onClick={handleToken}>Signup</button>
-            </> : <>
-                <button onClick={handleSignup}>Get OTP</button>
-            </>}
-        </form>
-        <p>{message}</p>
-    </>
+                <form className={styles.form}>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Your Name"
+                    />
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email"
+                    />
+
+                    {OTPSent ? (
+                        <>
+                            <input
+                                type="number"
+                                value={OTP}
+                                onChange={(e) => setOTP(e.target.value)}
+                                placeholder="Enter OTP"
+                            />
+                            <button onClick={handleToken} className={styles.btn}>
+                                Signup
+                            </button>
+                        </>
+                    ) : (
+                        <button onClick={handleSignup} className={styles.btn}>
+                            Get OTP
+                        </button>
+                    )}
+                </form>
+
+                <p className={styles.signin}>
+                    Already have an account?? <a href="#">Sign in</a>
+                </p>
+
+                <p className={styles.message}>{message}</p>
+            </div>
+
+            <div className={styles.right}></div>
+        </div>
+    )
 }
+
 export default Signup
